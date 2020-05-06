6e39904b63c6a5cce0bbf7f96c502c0ffef5cc0f
chowdhury.mdadnan@gmail.com <chowdhury.mohammadadnan06@gmail.com>
2020-05-06 06:09:42 +0600

    Outputting list through map function

--- App.js
+++ App.js
@@ -3,14 +3,20 @@ import Ninjas from './Ninjas';
 
 
 class App extends Component {
+  state = {
+    ninjas : [
+      {name : "Adnan" , age:25 ,belt: 'black' , id:1 },
+      {name : "Karim" , age:26 ,belt: "blue" ,  id:2 },
+      {name : "Crystal", age:27,belt: "pink" ,  id:3 }
+    ]
+  }
   render(){
   return (
     <div className="App">
       <h1> My First react app!</h1>
       <p>Welcome :)</p>
-      <Ninjas name = "Adnan" age ="25" belt= "black" />
-      <Ninjas name = "Karim" age ="26" belt= "blue" />
-      <Ninjas name = "Babil" age ="27" belt= "White" />
+      <Ninjas ninjas = {this.state.ninjas}/>
+      
     </div>
   );
 }
